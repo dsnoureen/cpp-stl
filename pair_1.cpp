1b4047c2f4f8c7d74d165a9bd58a782bd17b2e45
Dewan Sadia Noureen <dewansadianoureen02@gmail.com>
2025-12-03 00:50:34 +0600

    pair again

--- pair_1.cpp
+++ pair_1.cpp
@@ -5,11 +5,12 @@ int main(){
     cin>>n;
     pair<string,int>person[n];
     for(int i=0;i<n;i++){
+        string name;
+        int age;
+        person[i].first=name;
+        person[i].second=age;
         cin>>person[i].first>>person[i].second;
-    }
-    for(int i=0;i<n;i++){
-        cout<<person[i].first<<" "<<person[i].second; 
-        cout<<"\n";
+        cout<<person[i].first<<" "<<person[i].second<<"\n";
     }
     return 0;
 }
